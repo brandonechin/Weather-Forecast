68784697d31a660e8c7caab3b3c43a8d785f1429
Brandon Chin <bbrandonchin@yahoo.com>
2023-01-21 00:34:10 +0000

    Added loading page and hover psuedo classes to anchors and buttons.

--- js/data.js
+++ js/data.js
@@ -8,14 +8,14 @@ var data = {
   nextEntryId: 1
 };
 
-window.addEventListener('beforeunload', storage);
-function storage(event) {
-  var dataJson = JSON.stringify(data);
-  localStorage.setItem('javascript-local-storage', dataJson);
-}
+// window.addEventListener('beforeunload', storage);
+// function storage(event) {
+//   var dataJson = JSON.stringify(data);
+//   localStorage.setItem('javascript-local-storage', dataJson);
+// }
 
-var previousInputs = localStorage.getItem('javascript-local-storage');
+// var previousInputs = localStorage.getItem('javascript-local-storage');
 
-if (previousInputs !== null) {
-  data = JSON.parse(previousInputs);
-}
+// if (previousInputs !== null) {
+//   data = JSON.parse(previousInputs);
+// }
